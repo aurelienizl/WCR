111bf89e97c8ad71f2a4ce18424670be62b33ce4
Izoulet Aurélien <aurelien.izoulet@epita.fr>
2022-11-07 22:55:17 +0100

    Improved overall performance

--- Win32ClassModule/Win32ClassModule/Program.cs
+++ Win32ClassModule/Win32ClassModule/Program.cs
@@ -1,59 +1,165 @@
-﻿using System.Net;
+﻿using System.Threading;
+using WindowsReportingClient;
 using WindowsReportingClient.System;
 using WindowsReportingClient.Win32_Class;
 using WindowsReportingClient.Win32_Modules;
+
 
 namespace WindowsReportingClient;
 
 public static class Program
 {
-    private static string _host = "10.219.13.132";
-    private const int _port = 443;
 
     public static void Main()
     {
         InitReportingTool();
-        Networking.UploadReport(_host, _port);
+        InitNetwork();
+        InitUI();
     }
 
+    #region UI
+
+    public static void InitUI()
+    {
+        new MainWindow().ShowDialog();
+    }
+
+    #endregion
+
+    #region Network
+    private static Thread? NetworkThread;
+    public static void InitNetwork()
+    {
+
+        ReportingThread?.Join(); //Wait reporter to finished
+
+        if (NetworkThread is null)
+        {
+            NetworkThread = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                Networking.UploadReport("127.0.0.1", 443);
+            });
+            NetworkThread.Start();
+            return;
+        }
+        if (!NetworkThread.IsAlive)
+        {
+            NetworkThread = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                Networking.UploadReport("127.0.0.1", 443);
+            });
+            NetworkThread.Start();
+        }
+    }
+    #endregion
+
+    #region Report
+    private static List<Win32_Bios>? bios { get; set; }
+    private static List<Win32_EncryptableVolume>? win32_EncryptableVolumes { get; set; }
+    private static List<Win32_Tpm>? win32_Tpm { get; set; }
+    private static List<Win32_Product>? win32_Products { get; set; }
+    private static List<X509Cert>? X509CertList { get; set; }
+    private static List<Win32_QuickFixEngineering>? win32_QFE { get; set; }
+    private static List<Account>? accounts { get; set; }
+    private static SystemInfo? sysinfo { get; set; }
+    private static Thread? ReportingThread;
     public static void InitReportingTool()
     {
-        var bios = Win32_Bios.GetBios();
-        if (bios != null) PrintData.PrintBios(bios);
-
-        var win32_EncryptableVolumes =
-            Win32_EncryptableVolume.GetEncryptableVolume();
-        if (win32_EncryptableVolumes != null) PrintData.PrintEncryptableVolume(win32_EncryptableVolumes);
-
-        var win32_Tpm = Win32_Tpm.GetTpm();
-        if (win32_Tpm != null) PrintData.PrintTpm(win32_Tpm);
-
-        var win32_Products = Win32_Product.GetProducts();
-        if (win32_Products != null) PrintData.PrintProducts(win32_Products);
-
-        var X509CertList = X509Cert.GetX509Cert();
-        if (X509CertList != null) PrintData.PrintCerts(X509CertList);
-
-        var win32_QFE = Win32_QuickFixEngineering.GetQuickFixEngineering();
-        if (win32_QFE != null) PrintData.printQFE(win32_QFE);
-
-        var accounts = Account.GetLocalUsers();
-        if (accounts != null) PrintData.PrintAccounts(accounts);
-
-        var sysinfo = SystemInfo.GetSystemInfo();
-        if (sysinfo != null) PrintData.PrintSystemInfo(sysinfo);
-
-        var report = new Report(
-            bios,
-            win32_EncryptableVolumes,
-            win32_Tpm,
-            win32_Products,
-            X509CertList,
-            win32_QFE,
-            accounts,
-            sysinfo
-        );
-
-        Report.GenerateReport(report);
+        if (ReportingThread is null)
+        {
+            LaunchReport();
+            return;
+        }
+        if (!ReportingThread.IsAlive)
+        {
+            LaunchReport();
+            return;
+        }
+        MessageBox.Show("Reporting tool is running");
     }
+    public static void LaunchReport()
+    {
+        ReportingThread = new Thread(() =>
+        {
+            Thread.CurrentThread.IsBackground = true;
+            List<Thread> threads = new List<Thread>();
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                bios = Win32_Bios.GetBios();
+            }));
+
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                win32_EncryptableVolumes = Win32_EncryptableVolume.GetEncryptableVolume();
+            }));
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                win32_Tpm = Win32_Tpm.GetTpm();
+            }));
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                win32_Products = Win32_Product.GetProducts();
+            }));
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                X509CertList = X509Cert.GetX509Cert();
+            }));
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                win32_QFE = Win32_QuickFixEngineering.GetQuickFixEngineering();
+            }));
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                accounts = Account.GetLocalUsers();
+            }));
+
+            threads.Add(new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                sysinfo = SystemInfo.GetSystemInfo();
+            }));
+
+
+            foreach (var item in threads)
+            {
+                item.Start();
+            }
+            foreach (var item in threads)
+            {
+                item.Join();
+            }
+
+            var report = new Report(
+                bios,
+                win32_EncryptableVolumes,
+                win32_Tpm,
+                win32_Products,
+                X509CertList,
+                win32_QFE,
+                accounts,
+                sysinfo
+            );
+
+            Report.GenerateReport(report);
+        });
+        ReportingThread.Start();
+    }
+    #endregion
+
 }
